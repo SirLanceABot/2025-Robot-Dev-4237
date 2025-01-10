75204e70e6398759ace81a1abf1c922d3de9da27
Team 4237 Laptop 3 <LakeshoreRoboticsTeam@gmail.com>
2025-01-09 20:16:27 -0500

    GH NP added positions to shoulder :)

--- src/main/java/frc/robot/subsystems/Shoulder.java
+++ src/main/java/frc/robot/subsystems/Shoulder.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems;
 
 import java.lang.invoke.MethodHandles;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLimitSwitch;
+
 import frc.robot.Constants;
 import frc.robot.motors.TalonFXLance;
 
@@ -23,6 +26,8 @@ public class Shoulder extends SubsystemLance
     // *** INNER ENUMS and INNER CLASSES ***
     // Put all inner enums and inner classes here
 
+
+
     private class PeriodicData
     {
         // INPUTS
@@ -38,6 +43,10 @@ public class Shoulder extends SubsystemLance
     // *** CLASS VARIABLES & INSTANCE VARIABLES ***
     // Put all class variables and instance variables here
     private final PeriodicData periodicData = new PeriodicData();
+    
+    private SparkLimitSwitch forwardLimitSwitch;
+    private SparkLimitSwitch reverseLimitSwitch;
+    
     private final TalonFXLance motor1 = new TalonFXLance(4, Constants.ROBORIO, "Motor 1");
     private final TalonFXLance motor2 = new TalonFXLance(12, Constants.ROBORIO, "Motor 2");
 
@@ -54,6 +63,7 @@ public class Shoulder extends SubsystemLance
         System.out.println("  Constructor Started:  " + fullClassName);
 
         configMotors();
+        // relativeEncoder.setPosition(Constants.Shoulder.STARTING_POSITION);
 
         // SendableRegistry.addLW(this, "Example Subsystem", "MY Subsystem");
         // addChild("Motor 1", motor1);
@@ -70,6 +80,8 @@ public class Shoulder extends SubsystemLance
     {
         motor1.setupFactoryDefaults();
         motor2.setupFactoryDefaults();
+
+        //add limit switch here
     }
 
     /**
@@ -86,13 +98,65 @@ public class Shoulder extends SubsystemLance
         periodicData.motorSpeed = 0.0;
     }
 
+    // public void resetEncoder()
+    // {
+    //     resetState = ResetState.kStart;
+    // }
+
+    // /** @return encoder ticks (double) */
+    // public double getPosition() // encoder ticks
+    // {
+    //     return periodicIO.currentPosition;
+    // }
+
+    // /** @return angle (double) */
+    // public double getAngle()
+    // {
+    //     return periodicIO.currentAngle;
+    // }
+
+    // /** move the shoulder to Level 1 */
+    // public void L1()
+    // {
+    //     targetPosition = TargetPosition.kL1;
+    // }
+
+    // /** move the shoulder to Level 2*/
+    // public void L2()
+    // {
+    //     targetPosition = TargetPosition.kL2;
+    // }
+
+    // /** move the shoulder to Level 3 */
+    // private void L3()
+    // {
+    //     targetPosition = TargetPosition.kL3;
+    // }
+
+    // /** move the shoulder to Level 4 */
+    // private void L4()
+    // {
+    //     targetPosition = TargetPosition.kL4;
+    // }
+
+    // /** move the shoulder to Starting Position */
+    // private void StartingPosition()
+    // {
+    //     targetPosition = TargetPosition.kStartingPosition;
+    // }
+
+    // /** move the shoulder to Grab Coral Position */
+    // private void GrabCoralPosition()
+    // {
+    //     targetPosition = TargetPosition.kGrabCoralPosition;
+    // }
 
     // *** OVERRIDEN METHODS ***
     // Put all methods that are Overridden here
     @Override
     public void readPeriodicInputs()
     {
-
+        
     }
 
     @Override
