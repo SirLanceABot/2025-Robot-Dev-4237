efb2626bba0e4c8ac0662a479ab6ccdc64b6dee5
Team 4237 Laptop 3 <LakeshoreRoboticsTeam@gmail.com>
2025-01-11 12:43:44 -0500

    NP: clarified left and right motor ports

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,7 +63,8 @@ public final class Constants
 
     public static class Shoulder
     {
-        public static final int MOTOR_PORT                              = 0;
+        public static final int LEFT_MOTOR_PORT                         = 0;
+        public static final int RIGHT_MOTOR_PORT                        = 0;
         public static final String MOTOR_CAN_BUS                        = CANIVORE;
 
         public static final double L4                                   = 0.0;
