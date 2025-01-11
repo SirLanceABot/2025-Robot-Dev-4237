9fd982d6a87146c714174158285794665d1cdb05
Matthew Fontecchio <26mfontecchio@lpslancer.net>
2025-01-11 14:35:12 -0500

    MF/AY - Worked on Drivetrain

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -12,7 +12,9 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants;
+import frc.robot.controls.AdaptiveSlewRateLimiter;
 import frc.robot.sensors.GyroLance;
 
 public class Drivetrain extends SubsystemLance
@@ -32,27 +34,11 @@ public class Drivetrain extends SubsystemLance
     // *** INNER ENUMS and INNER CLASSES ***
     // Put all inner enums and inner classes here
 
-    private enum DriveMode
-    {
-        kDrive, kLockwheels, kStop, kArcadeDrive;
-    }
 
-    public enum ArcadeDriveDirection
-    {
-        kStraight(0.0), kStrafe(90.0);
-
-        public double value;
-        
-        private ArcadeDriveDirection(double value)
-        {
-            this.value =  value;
-        }
-    }
 
 
     // *** CLASS VARIABLES & INSTANCE VARIABLES ***
     // Put all class variables and instance variables here
-    private DriveMode driveMode = DriveMode.kDrive;
 
     //TODO Find out wheelbase and trackwidth values
     //Location of modules relative to robot center
@@ -80,9 +66,10 @@ public class Drivetrain extends SubsystemLance
     private final GyroLance gyro;
     private final SwerveDriveKinematics kinematics;
 
-    private double xSpeed;
-    private double ySpeed;
-    private double turnSpeed;
+    //Acceleration and decceleration limits (10.0 is last year's value)
+    private final AdaptiveSlewRateLimiter adaptiveXRateLimiter = new AdaptiveSlewRateLimiter(10.0, 10.0);
+    private final AdaptiveSlewRateLimiter adaptiveYRateLimiter = new AdaptiveSlewRateLimiter(10.0, 10.0);
+
     private SwerveModulePosition frontLeftPosition;
     private SwerveModulePosition frontRightPosition;
     private SwerveModulePosition backLeftPosition;
@@ -91,8 +78,6 @@ public class Drivetrain extends SubsystemLance
     private ChassisSpeeds chassisSpeeds;
     private SwerveDriveOdometry odometry;
     private SwerveModuleState[] inputSwerveModuleStates;
-    private boolean fieldRelative = true;
-
     
     
 
@@ -140,9 +125,7 @@ public class Drivetrain extends SubsystemLance
 
 
     private void lockWheels()
-    {
-        inputSwerveModuleStates = new SwerveModuleState[4];
-                
+    {                
         inputSwerveModuleStates[0] = new SwerveModuleState(0.0, Rotation2d.fromDegrees(45));
         inputSwerveModuleStates[1] = new SwerveModuleState(0.0, Rotation2d.fromDegrees(135));
         inputSwerveModuleStates[2] = new SwerveModuleState(0.0, Rotation2d.fromDegrees(135));
@@ -150,23 +133,7 @@ public class Drivetrain extends SubsystemLance
 
         setSwerveModuleStates(inputSwerveModuleStates);
     }
-
-    private void setSwerveModuleStates(SwerveModuleState[] states)
-    {
-        frontLeft.setDesiredState(states[0]);
-        frontRight.setDesiredState(states[1]);
-        backLeft.setDesiredState(states[2]);
-        backRight.setDesiredState(states[3]);
-    }
-
-
-    public void driveRobotRelative(ChassisSpeeds chassisSpeeds)
-    {
-        driveMode = DriveMode.kDrive;
-        inputSwerveModuleStates = kinematics.toSwerveModuleStates(chassisSpeeds);
-        //TODO: Find out max speed, 10 was last year's value
-        SwerveDriveKinematics.desaturateWheelSpeeds(inputSwerveModuleStates, 10.0);
-    }
+    
 
     private void drive(double xSpeed, double ySpeed, double turnSpeed, boolean fieldRelative, boolean useSlewRateLimiter)
     {
@@ -179,8 +146,8 @@ public class Drivetrain extends SubsystemLance
 
         if(useSlewRateLimiter)
         {
-            // xSpeed = adaptiveXRateLimiter.calculate(xSpeed);
-            // ySpeed = adaptiveYRateLimiter.calculate(ySpeed);
+            xSpeed = adaptiveXRateLimiter.calculate(xSpeed);
+            ySpeed = adaptiveYRateLimiter.calculate(ySpeed);
         }
     
 
@@ -196,6 +163,15 @@ public class Drivetrain extends SubsystemLance
         SwerveDriveKinematics.desaturateWheelSpeeds(inputSwerveModuleStates, 10.0);
 
         setSwerveModuleStates(inputSwerveModuleStates);
+    }
+
+
+    private void setSwerveModuleStates(SwerveModuleState[] states)
+    {
+        frontLeft.setDesiredState(states[0]);
+        frontRight.setDesiredState(states[1]);
+        backLeft.setDesiredState(states[2]);
+        backRight.setDesiredState(states[3]);
     }
 
 
@@ -215,71 +191,26 @@ public class Drivetrain extends SubsystemLance
     public Command driveCommand(DoubleSupplier xSpeed, DoubleSupplier ySpeed, DoubleSupplier turn, DoubleSupplier scale)
     {
         return
-        this.runOnce(
+        Commands.runOnce(
                 () -> drive(
                     xSpeed.getAsDouble() * scale.getAsDouble(),
                     ySpeed.getAsDouble() * scale.getAsDouble(),
                     turn.getAsDouble() * scale.getAsDouble(), 
                     true,
-                    true)
+                    true), this
                 )
                 .withName("driveCommand()");
-            }
+    }
 
-     @Override
-    public void readPeriodicInputs() 
-    {}
-
-    @Override
-    public void writePeriodicOutputs() 
-    {}
+    public Command lockWheelsCommand()
+    {
+        return Commands.runOnce(() -> lockWheels(), this).withName("lockWheelsCommand()");
+    }
 
     @Override
     public void periodic()
-    {
-        switch (driveMode)
-        {
-            case kDrive:
+    {   
 
-                if(fieldRelative)
-                    chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, turnSpeed, gyro.getRotation2d());
-                else
-                    chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turnSpeed);
-
-                //Using Chassis speeds
-                inputSwerveModuleStates = kinematics.toSwerveModuleStates(chassisSpeeds);
-
-                //Makes sure wheel speeds aren't too high
-                SwerveDriveKinematics.desaturateWheelSpeeds(inputSwerveModuleStates, 10.0);
-                break;
-
-            case kLockwheels:
-
-                inputSwerveModuleStates = new SwerveModuleState[4];
-                
-                inputSwerveModuleStates[0] = new SwerveModuleState(0.0, Rotation2d.fromDegrees(45));
-                inputSwerveModuleStates[1] = new SwerveModuleState(0.0, Rotation2d.fromDegrees(135));
-                inputSwerveModuleStates[2] = new SwerveModuleState(0.0, Rotation2d.fromDegrees(135));
-                inputSwerveModuleStates[3] = new SwerveModuleState(0.0, Rotation2d.fromDegrees(45));
-                break;
-
-            case kArcadeDrive:
-                //TODO: Find out max speed, 10 was last year's value
-                //Makes sure wheel speeds aren't too high
-                SwerveDriveKinematics.desaturateWheelSpeeds(inputSwerveModuleStates, 10.0);
-                break;
-
-            case kStop:
-                //No calculations to do
-                break;
-
-            
-        }
-        
-        frontLeft.setDesiredState(inputSwerveModuleStates[0]);
-        frontRight.setDesiredState(inputSwerveModuleStates[1]);
-        backLeft.setDesiredState(inputSwerveModuleStates[2]);
-        backRight.setDesiredState(inputSwerveModuleStates[3]);
     }
 
     @Override
