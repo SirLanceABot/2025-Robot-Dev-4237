fbe857f227c232fc6e1422da1cd5a9a97cefa1ee
Team 4237 Laptop 3 <LakeshoreRoboticsTeam@gmail.com>
2025-02-17 15:36:17 -0500

    TJ FIxed Field and added Alert code •ᴗ•

--- src/main/java/frc/robot/util/AutonomousTab.java
+++ src/main/java/frc/robot/util/AutonomousTab.java
@@ -4,7 +4,20 @@ package frc.robot.util;
 import java.lang.invoke.MethodHandles;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import edu.wpi.first.math.estimator.PoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
 
 // import com.pathplanner.lib.auto.AutoBuilder;
 
@@ -16,6 +29,7 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.util.AutoCommandList;
 // import frc.robot.RobotContainer;
 import frc.robot.util.AutonomousTabData.Right_Wall;
@@ -34,6 +48,7 @@ public class AutonomousTab
         System.out.println("Loading: " + fullClassName);
     }
 
+
     private enum ButtonState
     {
         kPressed, kStillPressed, kReleased, kStillReleased
@@ -46,6 +61,12 @@ public class AutonomousTab
     // Create the Autonomous Command List that will be scheduled to run during autonomousInit()
     // private Command autonomousCommand = null;
     // private RobotContainer robotContainer;
+    private Field2d autofield = new Field2d();
+    private Field2d field = new Field2d();
+    private CommandSwerveDrivetrain drivetrain;
+    private PoseEstimator poseEstimator;
+    private Trajectory trajectory;
+    private Alert autonomousAlert;
     
     
   
@@ -82,12 +103,30 @@ public class AutonomousTab
         createLeftWallBox();
         createRightWallBox();
         createMiddleBox();
+
+        SmartDashboard.putData("AutoField", autofield);
+        SmartDashboard.putData("Field", field);
         //createSitPrettyBox();
         // createScoreMoreNotesBox();
         // createDriveOutOfStartZoneBox();
+        autonomousAlert = new Alert("Logan Likes Rainbows", AlertType.kWarning);
 
-
-    
+        // Create the trajectory to follow in autonomous. It is best to initialize
+            // trajectories here to avoid wasting time in autonomous.
+            trajectory =
+                TrajectoryGenerator.generateTrajectory(
+                    new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+                    List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                    new Pose2d(3, 0, Rotation2d.fromDegrees(0)),
+                    new TrajectoryConfig(Units.feetToMeters(3.0), Units.feetToMeters(3.0)));
+        
+            // Create and push Field2d to SmartDashboard.
+            SmartDashboard.putData(autofield);
+       
+        
+            // Push the trajectory to Field2d.
+            autofield.getObject("traj").setTrajectory(trajectory);
+            SmartDashboard.putData(field);
         
         // createSendDataButton();
         // successfulDownload = createSuccessfulDownloadBox();
